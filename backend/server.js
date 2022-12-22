e5a7e282f087a445a6c81cca6e017da33097eeee
Rahul Aggarwal <rahul.aggarwal000005@gmail.com>
2022-12-22 11:45:40 +0530

    Initialize backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const notes = require("./data/notes");
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+app.get("/api/notes", (req, res) => {
+  res.send(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((note) => note._id == req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${5000}`);
+});
